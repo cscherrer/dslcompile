c5ba308f23d42d09501d3b80609dd53994e00c2e
Chad Scherrer <chad.scherrer@gmail.com>
2025-06-09 16:19:34 -0700

    cleanup

--- dslcompile/src/symbolic/mod.rs
+++ dslcompile/src/symbolic/mod.rs
@@ -3,7 +3,5 @@ pub mod native_egglog;
 pub mod power_utils;
 pub mod rule_loader;
 /// Symbolic optimization and analysis
-// pub mod summation; // Removed - functionality moved to DynamicContext.sum()
 pub mod symbolic;
 pub mod symbolic_ad;
-// pub mod collection_summation; // TODO: Reimplemented to work with new Sum(Collection) format
